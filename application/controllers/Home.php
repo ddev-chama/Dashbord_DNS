8f33eb69b6fab55f025cce5dd3a688c106831d27
unknown <ddev.chama@gmail.com>
2019-02-12 22:34:51 +0700

    old file

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -16,27 +16,4 @@ class Home extends CI_Controller {
         $this->load->view('login');
         //load view and pass the data
     }
-    public function login()  
-    {   $user = $this->input->post('user');  
-        $pass = $this->input->post('pass');  
-        $data['member'] = $this->home_model->get_member($user,$pass);
-        
-        if ($user=='sushi' && $pass=='123')   
-        {  
-            //declaring session  
-            $this->session->set_userdata(array('user'=>$user));  
-            
-        }  
-        else{  
-            $data['error'] = 'Your Account is Invalid';  
-            $this->load->view('login');  
-        }  
-    }
-    public function logout()
-    {
-        $this->session->unset_userdata('user');     
-        $this->session->sess_destroy();
-        $this->load->view('login');  
-        
-    }
 }
